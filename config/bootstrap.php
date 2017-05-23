004d6a66e7cc966abd6191da419b956722052c32
fm-labs <flowmotionlabs@gmail.com>
2017-05-24 01:13:17 +0200

    removed auto-loading of default tcpdf config

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,16 +1,1 @@
 <?php
-use Cake\Core\Configure;
-use Cake\Log\Log;
-/*
- * Autoload TCPDF configuration
- */
-try {
-    Configure::load('tcpdf');
-
-} catch (\Exception $ex) {
-    Log::warning("Tcpdf: Failed to load configuration: " . $ex->getMessage());
-
-    if (Configure::read('debug')) {
-        throw $ex;
-    }
-}
